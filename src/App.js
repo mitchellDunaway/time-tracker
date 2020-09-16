d167c68121d5c2d6671578a4831c8a940ed0c8e3
mitchellDunaway <warui.nioi@gmail.com>
2020-09-16 12:21:49 -0500

    shrug

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import Home from "./components/Home";
 import './App.css';
 
 function App() {
+
+	
+
 	return (
-		<BrowserRouter>
-			<div className="app">
-				<Route component={Home} path="/" exact />
-			</div>
-		</BrowserRouter>
+		// <BrowserRouter>
+		// 	<div className="app">
+		// 		<Route component={Home} path="/" exact />
+		// 	</div>
+		// </BrowserRouter>
+		newFunction()
 	);
+
+	function newFunction() {
+		console.log("blegh");
+		return <div>smello world</div>;
+	}
 }
 
 export default App;
